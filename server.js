b959635f6c240f3a071e9c7c0e01c906095fc94a
drtaupier <drtaupier@gmail.com>
2020-02-18 12:17:33 -0500

    My first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+//Express to run server and routes
+const express = require('express');
+//Start up an instance of app
+const app = express();
+
+//Dependencies
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+//Inicialize tha main project folder
+app.use(express.static('/', 'website'));
+
+const port = 3000;
+//Spin up the server
+const server = app.listen(port, listening);
+
+//callback
+function listening(){
+    console.log(`Running in the localhost: ${port}` );
+};
+
+
