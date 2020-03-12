5ab3d018a078a3ee9a863d930a4e291dfffaf1e0
drtaupier <drtaupier@gmail.com>
2020-03-12 13:37:15 -0400

    Now I can look for by zip code

--- website/app.js
+++ website/app.js
@@ -8,14 +8,14 @@ window.onload = function(){
             footer.innerHTML = `<h3> Copyright &copy; ${year} </h3>`;
         }
 
-        resultados(city, temperature, description, country){
+        resultados(city, temperature, country, dateNow){
             const article = document.getElementById('article');
             const element = document.createElement('div');
             element.classList.add('resultados');
             element.innerHTML = `<h2>${city}</h2>`;
             element.innerHTML += `<p>Country: ${country}</p>`;
             element.innerHTML += `<p>Temperature: ${temperature} &#176;F</p>`;
-            element.innerHTML += `<p>Description: ${description}</p>`;
+            element.innerHTML += `<p>Last Update: ${dateNow}</p>`;
             article.appendChild(element);
         }
 
@@ -34,14 +34,12 @@ window.onload = function(){
             const year = today.getFullYear();
             const hour = today.getHours();
             const minutes = today.getMinutes();
-            const fechaActual = `${month}/${day}/${year}`;
-            console.log(fechaActual);
+            const fechaActual = `${month}-${day}-${year} ${hour}:${minutes} hrs.`;
+            return fechaActual;
         }
 
     }
     
-    
-
     //Instanciando la clase
     const ui = new UI();
     
@@ -49,32 +47,33 @@ window.onload = function(){
     ui.footerCopyright();
     //Variables:
     const form = document.getElementById('form');
-    const ciudad = document.getElementById('city');
+    const zipCode = document.getElementById('zip');
 
     //function to change to uppercase.
-    ciudad.onkeyup = function(){
-        ciudad.value = ciudad.value.toUpperCase();
+    zipCode.onkeyup = function(){
+        zipCode.value = zipCode.value.toUpperCase();
     }
 
     form.addEventListener('submit', function(e){        
         ui.delete();
         const apiKey = 'b06beb7d88106fd35742f31d9b9865b5';
-        const inputValue = document.getElementById('city').value;
+        const inputValue = document.getElementById('zip').value;
+        const pais = 'US';
         e.preventDefault();
-        fetch(``)
+        fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${inputValue},${pais}&appid=${apiKey}`)
         .then(response => response.json())
         .then(data => {
             const city = data['name'];
             const temperature2 = data['main']['temp'];
             const temperature = Math.round((temperature2*1.8)-459.67); //convertir kelvin a Fahrenheit y redondeamos el resultado
-            const descValue = data['weather'][0]['description'];
             const country = data['sys']['country'];
+            const dateNow = ui.date();
         
-            ui.resultados(city, temperature, descValue, country);
+            ui.resultados(city, temperature, country, dateNow);
             })
         
-        .catch(err => console.log('Wrong city name'))
-        }) 
+        .catch(err => console.log('Wrong city name', err))
+        })
     
     console.log(t0);   
 }
